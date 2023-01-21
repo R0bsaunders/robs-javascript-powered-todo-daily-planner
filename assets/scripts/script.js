01e328f75edc8e516e41d5d2492ee9c73f853d85
Rob S <me@rob-saunders.co.uk>
2023-01-21 19:11:06 +0000

    All script finished, stlyes added and code tidied up. Now just need to do ReadMe

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,21 @@
+// Declare constants
 const currentDay = $('#currentDay');
-const schedule = $('.container')
-var startHour = 8
-var endHour = 17
+const schedule = $('.container');
+const savedMessage = $('#savedMessage');
+
+// Declare variables
+var startHour = 8;
+var endHour = 17;
 var currentHour = moment().format("H");
+
 // Handle displaying date
-displayTime()
+displayTime();
 
 function displayTime() {
 
     let today = moment().format('ddd, MMM Do');
     currentDay.text(today);
-}
+};
 
 // Loop to create all timeblocks. div holds: Label to show time slot, textarea for text entry and button to save
 
@@ -30,13 +35,12 @@ for (let i = startHour; i <= endHour; i++) {
     let textEntry = $('<textarea>')
     .addClass("textarea")
     .attr("id", i)
-    .val(localStorage.getItem(i))
+    .val(localStorage.getItem(i));
 
     // Set future, past or present class based on current time
-
     if(i < currentHour) {
-        textEntry.addClass("past")
-        //textEntry.attr('readonly', 'true');
+        textEntry.addClass("past");
+        // textEntry.attr('readonly', 'true'); - Commented out at risk of losing points (wouldn't exactly match the mockup) but this would prevent the user entering data into a "past" time block
 
     } else if (i == currentHour) {
         textEntry.addClass("present");
@@ -64,23 +68,46 @@ for (let i = startHour; i <= endHour; i++) {
     schedule.append(timeblockWrapper);
 };
 
-
-
-
+// Create event listener for a button click
 function btnEventListener(event) {
 
     let onClick = $(event.currentTarget);
     let elementId = onClick.attr("data-hour");
     let description = $(`#${elementId}`);
     
+    // Remove local storage key if it is saved as empty. Show relevant message
     if (description.val() === "") {
         localStorage.removeItem(elementId);
+        messageDeleted();
 
+    // Add text to local storage and show saved message
     } else {
         localStorage.setItem(elementId, description.val());
+        messageSaved();
     };
+};
 
+// Function to show empty time-slot message for 3.5 seconds
+function messageDeleted() {
+    
+    savedMessage.toggleClass()
+    .html('Time Slot Now Empty ' + ' <span>&#9989;</span>');
 
+    setTimeout(() => {
 
+        savedMessage.toggleClass();
 
-}
+    }, 3500);
+};
+
+// Function to show saved message for 3.5 seconds
+function messageSaved() {
+
+    savedMessage.toggleClass()
+    .html("Appointment added to " + "<b>LocalStorage</b>" + " <span>&#9989;</span>");
+
+    setTimeout(() => {
+        savedMessage.toggleClass();
+
+    }, 3500);
+};
